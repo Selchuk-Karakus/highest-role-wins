8cde40e236388c1da5d30438c584d5457c9c4576
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-11-02 22:06:27 +0000

    chore: Configure server to render html file to the browser

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const http = require("http");
-http
-  .createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello World!!!");
-    response.end()
-  })
-  .listen(8080);
+const fs = require("fs");
+
+function req(request, response) {
+  fs.readFile("index.html", function (err, data) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(data);
+    response.end();
+  });
+}
+
+http.createServer(req).listen(8080);
