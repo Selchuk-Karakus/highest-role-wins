5029dad83b1a1d1e8015d170bd027c88b02c918d
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-11-04 22:26:01 +0000

    chore: Wire up data file to app and serve it on /users route

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const usersData = require("./data/users-data.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/users", (req, res) => {
+  res.send(usersData);
 });
 
 app.listen(PORT, () => {
